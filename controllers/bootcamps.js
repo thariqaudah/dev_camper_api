6e45b5dd9bfb51b1b762d5850bde0e7554bea829
Thariq Audah <thariq.audah@gmail.com>
2021-06-29 22:13:26 +0700

    Relate bootcamp with user

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,176 +8,193 @@ const Bootcamp = require('../models/Bootcamp');
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-	// Sending response
-	res.status(200).json(res.advancedResult);
+  // Sending response
+  res.status(200).json(res.advancedResult);
 });
 
 // @desc      Get single bootcamp
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
-	let bootcamp = await Bootcamp.findById(req.params.id).populate('courses');
+  let bootcamp = await Bootcamp.findById(req.params.id).populate('courses');
 
-	if (!bootcamp) {
-		return next(
-			new ErrorResponse(
-				`Resource with id of ${req.params.id} is not found`,
-				404
-			)
-		);
-	}
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Resource with id of ${req.params.id} is not found`,
+        404
+      )
+    );
+  }
 
-	res.status(200).json({
-		success: true,
-		data: bootcamp,
-	});
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
 });
 
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-	const bootcamp = await Bootcamp.create(req.body);
-	res.status(201).json({
-		success: true,
-		data: bootcamp,
-	});
+  // Add user id to req.body
+  req.body.user = req.user._id;
+
+  // Check for published bootcamp by the user
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user._id });
+
+  // If user is not an admin, they just could publish one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User with id of ${req.user._id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 });
 
 // @desc      Update bootcamps
 // @route     PUT /api/v1/bootcamps/:id
 // @access    Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-	const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
-		runValidators: true,
-	});
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-	if (!bootcamp) {
-		return next(
-			new ErrorResponse(
-				`Resource with id of ${req.params.id} is not found`,
-				404
-			)
-		);
-	}
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Resource with id of ${req.params.id} is not found`,
+        404
+      )
+    );
+  }
 
-	res.status(200).json({
-		success: true,
-		data: bootcamp,
-	});
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
 });
 
 // @desc      Delete bootcamps
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-	const bootcamp = await Bootcamp.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-	if (!bootcamp) {
-		return next(
-			new ErrorResponse(
-				`Resource with id of ${req.params.id} is not found`,
-				404
-			)
-		);
-	}
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Resource with id of ${req.params.id} is not found`,
+        404
+      )
+    );
+  }
 
-	await bootcamp.remove();
+  await bootcamp.remove();
 
-	res.status(200).json({
-		success: true,
-		data: {},
-	});
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 });
 
 // @desc      Get Bootcamps within a radius
 // @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
 // @access    Private
 exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
-	const { zipcode, distance } = req.params;
+  const { zipcode, distance } = req.params;
 
-	// Get lat/lng from geocoder
-	const loc = await geocoder.geocode(zipcode);
-	const lat = loc[0].latitude;
-	const lng = loc[0].longitude;
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
 
-	// Calc radius by radians
-	// Divided by radius of Earth
-	// Radius of Earth is 3963 mi / 6378 km
-	const radius = distance / 3963;
+  // Calc radius by radians
+  // Divided by radius of Earth
+  // Radius of Earth is 3963 mi / 6378 km
+  const radius = distance / 3963;
 
-	// Find bootcamps
-	const bootcamps = await Bootcamp.find({
-		location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
-	});
+  // Find bootcamps
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
 
-	res.status(200).json({
-		success: true,
-		count: bootcamps.length,
-		data: bootcamps,
-	});
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
 
 // @desc      PUT Upload bootcamp photo
 // @route     GET /api/v1/bootcamps/:id/photo
 // @access    Private
 exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
-	let bootcamp = await Bootcamp.findById(req.params.id);
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
-	if (!bootcamp) {
-		next(
-			new ErrorResponse(
-				`Bootcamp with id of ${req.params.id} is not found`,
-				404
-			)
-		);
-	}
+  if (!bootcamp) {
+    next(
+      new ErrorResponse(
+        `Bootcamp with id of ${req.params.id} is not found`,
+        404
+      )
+    );
+  }
 
-	if (!req.files) {
-		next(new ErrorResponse(`Please upload a photo`, 400));
-	}
+  if (!req.files) {
+    next(new ErrorResponse(`Please upload a photo`, 400));
+  }
 
-	const photo = req.files.photo;
+  const photo = req.files.photo;
 
-	// Check if photo is an image file
-	if (!photo.mimetype.includes('image')) {
-		next(new ErrorResponse(`Please upload an image file`, 400));
-	}
+  // Check if photo is an image file
+  if (!photo.mimetype.includes('image')) {
+    next(new ErrorResponse(`Please upload an image file`, 400));
+  }
 
-	// Check if photo is less than max size
-	if (photo.size > process.env.PHOTO_MAX_SIZE) {
-		next(
-			new ErrorResponse(
-				`Photo file size should be less than ${process.env.PHOTO_MAX_SIZE} bytes`,
-				400
-			)
-		);
-	}
+  // Check if photo is less than max size
+  if (photo.size > process.env.PHOTO_MAX_SIZE) {
+    next(
+      new ErrorResponse(
+        `Photo file size should be less than ${process.env.PHOTO_MAX_SIZE} bytes`,
+        400
+      )
+    );
+  }
 
-	// Create custom file name
-	photo.name = `photo_${bootcamp._id}${path.parse(photo.name).ext}`;
+  // Create custom file name
+  photo.name = `photo_${bootcamp._id}${path.parse(photo.name).ext}`;
 
-	// Upload photo
-	photo.mv(`${process.env.PHOTO_FILE_PATH}/${photo.name}`, async err => {
-		if (err) {
-			console.error(err);
-			return next(
-				new ErrorResponse(`Something gone wrong with photo upload`, 500)
-			);
-		}
+  // Upload photo
+  photo.mv(`${process.env.PHOTO_FILE_PATH}/${photo.name}`, async (err) => {
+    if (err) {
+      console.error(err);
+      return next(
+        new ErrorResponse(`Something gone wrong with photo upload`, 500)
+      );
+    }
 
-		bootcamp = await Bootcamp.findByIdAndUpdate(
-			req.params.id,
-			{
-				photo: photo.name,
-			},
-			{ new: true, runValidators: true }
-		);
+    bootcamp = await Bootcamp.findByIdAndUpdate(
+      req.params.id,
+      {
+        photo: photo.name,
+      },
+      { new: true, runValidators: true }
+    );
 
-		res.status(200).json({
-			success: true,
-			data: bootcamp.photo,
-		});
-	});
+    res.status(200).json({
+      success: true,
+      data: bootcamp.photo,
+    });
+  });
 });
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -3,146 +3,151 @@ const slugify = require('slugify');
 const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: [true, 'Please enter a name'],
-			unique: [true, 'That name already exist'],
-			trim: true,
-			maxLength: [50, 'Name can not be greater than 50 characters'],
-		},
-		slug: String,
-		description: {
-			type: String,
-			required: [true, 'Please enter a description'],
-			maxLength: [500, 'Description can not be greater than 500 characters'],
-		},
-		website: {
-			type: String,
-			match: [
-				/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-				'Please enter a valid URL with HTTP or HTTPS',
-			],
-		},
-		phone: {
-			type: String,
-			maxLength: [20, 'Phone number can not be longer than 20 characters'],
-		},
-		email: {
-			type: String,
-			match: [
-				/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-				'Please enter a valid email address',
-			],
-		},
-		address: {
-			type: String,
-			required: [true, 'Please enter an address'],
-		},
-		location: {
-			// GeoJSON Point
-			type: {
-				type: String,
-				enum: ['Point'],
-			},
-			coordinates: {
-				type: [Number],
-				index: '2dsphere',
-			},
-			formattedAddress: String,
-			street: String,
-			city: String,
-			state: String,
-			zipcode: String,
-			country: String,
-		},
-		careers: {
-			type: [String],
-			required: true,
-			enum: [
-				'Web Development',
-				'Mobile Development',
-				'UI/UX',
-				'Data Science',
-				'Machine Learning',
-				'Business',
-				'Other',
-			],
-		},
-		averageRating: {
-			type: Number,
-			minLength: [1, 'Rating must be at least 1'],
-			maxLength: [10, 'Rating can not be greater than 10'],
-		},
-		averageCost: Number,
-		photo: {
-			type: String,
-			default: 'no-photo.jpg',
-		},
-		housing: {
-			type: Boolean,
-			default: false,
-		},
-		jobAssistance: {
-			type: Boolean,
-			default: false,
-		},
-		jobGuarantee: {
-			type: Boolean,
-			default: false,
-		},
-		acceptGi: {
-			type: Boolean,
-			default: false,
-		},
-		createdAt: {
-			type: Date,
-			default: Date.now,
-		},
-	},
-	{
-		toJSON: { virtuals: true },
-		toObject: { virtuals: true },
-	}
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter a name'],
+      unique: [true, 'That name already exist'],
+      trim: true,
+      maxLength: [50, 'Name can not be greater than 50 characters'],
+    },
+    slug: String,
+    description: {
+      type: String,
+      required: [true, 'Please enter a description'],
+      maxLength: [500, 'Description can not be greater than 500 characters'],
+    },
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        'Please enter a valid URL with HTTP or HTTPS',
+      ],
+    },
+    phone: {
+      type: String,
+      maxLength: [20, 'Phone number can not be longer than 20 characters'],
+    },
+    email: {
+      type: String,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please enter a valid email address',
+      ],
+    },
+    address: {
+      type: String,
+      required: [true, 'Please enter an address'],
+    },
+    location: {
+      // GeoJSON Point
+      type: {
+        type: String,
+        enum: ['Point'],
+      },
+      coordinates: {
+        type: [Number],
+        index: '2dsphere',
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String,
+    },
+    careers: {
+      type: [String],
+      required: true,
+      enum: [
+        'Web Development',
+        'Mobile Development',
+        'UI/UX',
+        'Data Science',
+        'Machine Learning',
+        'Business',
+        'Other',
+      ],
+    },
+    averageRating: {
+      type: Number,
+      minLength: [1, 'Rating must be at least 1'],
+      maxLength: [10, 'Rating can not be greater than 10'],
+    },
+    averageCost: Number,
+    photo: {
+      type: String,
+      default: 'no-photo.jpg',
+    },
+    housing: {
+      type: Boolean,
+      default: false,
+    },
+    jobAssistance: {
+      type: Boolean,
+      default: false,
+    },
+    jobGuarantee: {
+      type: Boolean,
+      default: false,
+    },
+    acceptGi: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
 
 // Pre middleware for create slug
 BootcampSchema.pre('save', function (next) {
-	this.slug = slugify(this.name, { remove: /[*+~.()'"!:@]/g, lower: true });
-	next();
+  this.slug = slugify(this.name, { remove: /[*+~.()'"!:@]/g, lower: true });
+  next();
 });
 
 // Pre middleware for save geocode location
 BootcampSchema.pre('save', async function (next) {
-	const loc = await geocoder.geocode(this.address);
-	this.location = {
-		type: 'Point',
-		coordinates: [loc[0].longitude, loc[0].latitude],
-		formattedAddress: loc[0].formattedAddress,
-		street: loc[0].streetName,
-		city: loc[0].city,
-		state: loc[0].stateCode,
-		zipcode: loc[0].zipcode,
-		country: loc[0].countryCode,
-	};
-	// Do not save the address field
-	this.address = undefined;
-	next();
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+  // Do not save the address field
+  this.address = undefined;
+  next();
 });
 
 // Virtual field of courses for bootcamp
 BootcampSchema.virtual('courses', {
-	ref: 'Course',
-	localField: '_id',
-	foreignField: 'bootcamp',
-	justOne: false,
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false,
 });
 
 // Cascade delete courses when a bootcamp is deleted
 BootcampSchema.pre('remove', async function (next) {
-	console.log(`Courses for bootcamp ${this._id} are deleted`);
-	await mongoose.model('Course').deleteMany({ bootcamp: this._id });
-	next();
+  console.log(`Courses for bootcamp ${this._id} are deleted`);
+  await mongoose.model('Course').deleteMany({ bootcamp: this._id });
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
